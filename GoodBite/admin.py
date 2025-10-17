8a2ed443abf797fb388f9f5044389f40c75b8a07
Oriol Ramos <oriol.ramos.puig@estudiantat.upc.edu>
2025-10-17 19:18:03 +0200

    add products to the admin and add seller info in products view

--- GoodBite/admin.py
+++ GoodBite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_by')
+    search_fields = ('name',)
+    list_filter = ('created_by',)
