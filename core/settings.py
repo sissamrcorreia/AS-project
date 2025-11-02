61bbe0f20762fbed47b69be3e7c39085f2217a3b
Oriol Ramos <oriol.ramos.puig@estudiantat.upc.edu>
2025-11-02 21:34:59 +0100

    fix password vulnerbility

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fr@nprd7cu5-b^jttcz9^emg*n&2d*c_&)pk#2eoanzso^vaon'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == '1'
 
 ALLOWED_HOSTS = [
     'localhost',
