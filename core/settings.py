5fee5dde1e10677139b7a8f7556a1704517634b7
Oriol Ramos <oriol.ramos.puig@estudiantat.upc.edu>
2025-11-03 23:39:18 +0100

    render.com fix

--- core/settings.py
+++ core/settings.py
@@ -136,11 +136,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'GoodBite' / 'static_seed_images',
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'GoodBite' / 'static_seed_images',
+#]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
