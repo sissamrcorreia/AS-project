2b20def96e394c7ef6c659165138addde2a7ceea
anna.melkumyam <anna.melkumyam@estudiantat.upc.edu>
2025-11-10 16:28:39 +0100

    problem of placeholder fixed

--- core/settings.py
+++ core/settings.py
@@ -136,14 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
-#STATICFILES_DIRS = [
-#    BASE_DIR / 'GoodBite' / 'static_seed_images',
-#]
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'GoodBite' / 'static_seed_images',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
